2bf860c69b3e992cc0349c9d6d9c282417ae270b
Arakawa <lylaaaaaa@gmail.com>
2021-01-24 20:44:18 -0600

    one minute label / ten seconds label added. Practically done all the function of the timer.

--- SampleAppMicrowave/SampleAppMicrowave/ViewController.swift
+++ SampleAppMicrowave/SampleAppMicrowave/ViewController.swift
@@ -12,21 +12,37 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     @IBOutlet weak var minLabel: UILabel!
     @IBOutlet weak var secLabel: UILabel!
+    @IBOutlet weak var oneMinLabel: UIButton!
+    @IBOutlet weak var tenSecLabel: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     @IBOutlet weak var pickerView: UIPickerView!
-    let list1:[String] = ["0", "1", "2", "3", "4", "5","6", "7", "8","9", "10", "11", "12", "13", "14", "15"]
+    
+    let list1:[String] = ["0", "1", "2", "3", "4", "5","6", "7", "8","9", "10"]
     let list2:[String] = ["00", "10", "20", "30", "40", "50"]
     
     var timer = Timer()
     var count = 0
     var minRemain = 0
     var secRemain = 0
+    var stopButtonTapCount = 0
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        oneMinLabel.layer.cornerRadius = 25
+        tenSecLabel.layer.cornerRadius = 25
+        stopButton.layer.borderWidth = 0.5
+        stopButton.layer.borderColor = UIColor.red.cgColor
+        startButton.layer.borderWidth = 0.5
+        startButton.layer.borderColor = UIColor.red.cgColor
+
         pickerView.delegate = self
         pickerView.dataSource = self
+        resetTimer()
     }
     
     //ドラムロールの列数
@@ -69,19 +85,41 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             break
         }
     }
+    
+    @IBAction func didTapOneMinuteButton(_ sender: UIButton) {
+        if timer.isValid == true {
+            return
+        }
+        timer.invalidate()
+        setTimer(minSelected: 1, secSelected: 0)
 
+    }
+    
+    @IBAction func didTapTenSecButton(_ sender: UIButton) {
+        if timer.isValid == true {
+            return
+        }
+        timer.invalidate()
+        setTimer(minSelected: 0, secSelected: 10)
+    }
+    
     @IBAction func startButton(_ sender: UIButton) {
+        //タイマーがオンだったら無反応
+        if timer.isValid == true {
+            return
+        }
         timer.invalidate()
         let minSelected = Int(list1[pickerView.selectedRow(inComponent: 0)])
         let secSelected = Int(list2[pickerView.selectedRow(inComponent: 1)])
-        count = minSelected! * 60 + secSelected!
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
-    }
+        setTimer(minSelected: minSelected!, secSelected: secSelected!)
+        
 
+            }
     
-
-    @IBAction func stopButton(_ sender: UIButton) {
-        timer.invalidate()
+    
+    func setTimer(minSelected: Int, secSelected: Int) {
+        count = minSelected * 60 + secSelected
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
     }
     
     //タイマーから呼び出されるメソッド
@@ -96,15 +134,30 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                 secLabel.text = String("0\(secRemain)")
             }
         }else{
-            minLabel.text = "00"
+            minLabel.text = "0"
             secLabel.text = "00"
             timer.invalidate()
         }
     }
     
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+        stopButtonTapCount += 1
+        timer.invalidate()
+        if stopButtonTapCount == 2 {
+            resetTimer()
+        }
+    }
 
     
-
+    func resetTimer() {
+        count = 0
+        stopButtonTapCount = 0
+        minLabel.text = "0"
+        secLabel.text = "00"
+        pickerView.selectRow(0, inComponent: 0, animated: true)
+        pickerView.selectRow(0, inComponent: 1, animated: true)
+    }
     
     }
 
